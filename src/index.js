92cde81daf807c4729d4788517b375ab7dd0c26b
Lexxby <iplexx@bk.ru>
2022-01-13 10:59:36 +0300

    add descision to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,92 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let openBrackets = '';
+  let closeBrackets = {};
+  if (bracketsConfig.length === 1 && bracketsConfig[0][0] === bracketsConfig[0][1]) {
+    return true;
+  }
+  for (i = 0; i < bracketsConfig.length; i++) {
+    openBrackets = openBrackets + bracketsConfig[i][0];
+    closeBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  let data = [];
+  let checkResult = true;
+  for (i = 0; i < str.length; i++) {
+    let lastSymbol = data[data.length - 1];
+    if (openBrackets.includes(str[i]) && data.length === 0) {
+      data.push(str[i]);
+    } else if (openBrackets.includes(str[i]) && closeBrackets[str[i]] !== lastSymbol) {
+      data.push(str[i]);
+    } else if (closeBrackets[str[i]] === lastSymbol) {
+      data.pop();
+    } else if (data.length === 0 && !openBrackets.includes(str[i])) {
+      checkResult = false;
+      break;
+    }
+  }
+  return data.length === 0 && checkResult ? true : false;
+};
+
+// const config1 = [['(', ')']];
+// const config2 = [
+//   ['(', ')'],
+//   ['[', ']']
+// ];
+// const config3 = [
+//   ['(', ')'],
+//   ['[', ']'],
+//   ['{', '}']
+// ];
+// const config4 = [['|', '|']];
+// const config5 = [
+//   ['(', ')'],
+//   ['|', '|']
+// ];
+// const config6 = [
+//   ['1', '2'],
+//   ['3', '4'],
+//   ['5', '6'],
+//   ['7', '7'],
+//   ['8', '8']
+// ];
+// const config7 = [
+//   ['(', ')'],
+//   ['[', ']'],
+//   ['{', '}'],
+//   ['|', '|']
+// ];
+
+// function check(str, bracketsConfig) {
+//   if (bracketsConfig[0][0] === bracketsConfig[0][1]) {
+//     return true;
+//   }
+//   openBrackets = '';
+//   closeBrackets = {};
+//   for (i = 0; i < bracketsConfig.length; i++) {
+//     openBrackets = openBrackets + bracketsConfig[i][0];
+//     closeBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+//   }
+//   console.log(bracketsConfig);
+//   console.log('openBrackets', openBrackets);
+//   console.log('closeBrackets', closeBrackets);
+
+//   let data = [];
+//   let checkResult = true;
+//   for (i = 0; i < str.length; i++) {
+//     let lastSymbol = data[data.length - 1];
+//     if (openBrackets.includes(str[i]) && data.length === 0) {
+//       data.push(str[i]);
+//       console.log(data);
+//     } else if (openBrackets.includes(str[i]) && closeBrackets[str[i]] !== lastSymbol) {
+//       data.push(str[i]);
+//     } else if (closeBrackets[str[i]] === lastSymbol) {
+//       data.pop();
+//     } else if (data.length === 0 && !openBrackets.includes(str[i])) {
+//       checkResult = false;
+//       break;
+//     }
+//   }
+//   return console.log(data.length === 0 && checkResult ? true : false);
+// }
+
+// check('((()))()', config1);
